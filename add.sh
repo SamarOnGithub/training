4aa64405eeea29749ee0bcead1d641ed42632a20
Samarjeet Mohanty <cloud_user@samar1c.mylabserver.com>
2019-02-19 01:46:54 +0000

    removed $ symbol fronting variables to enable proper addition

--- add.sh
+++ add.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# This is a comment
 echo "first number is $fistnum"
 echo "second number is $secondnum"
-sum=$(($firstnum + $secondnum))
+sum=$((firstnum + secondnum))
 echo "sum is $sum"
